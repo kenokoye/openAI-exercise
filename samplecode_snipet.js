5e2f6771c6535a43b32c5a70aab9f7b91c9a75a0
Kene Okoye <130005116+kenokoye@users.noreply.github.com>
2024-08-09 19:37:48 +0100

    Add files via upload

---
+++ samplecode_snipet.js
@@ -0,0 +1,12 @@
+// Saving data to localStorage
+localStorage.setItem('key', 'value');
+
+// Retrieving data from localStorage
+let storedValue = localStorage.getItem('key');
+console.log(storedValue); // Outputs: 'value'
+
+// Removing data from localStorage
+localStorage.removeItem('key');
+
+// Clearing all data from localStorage
+localStorage.clear();
